784b3c262fb9cbbb39ec662e9862365647ebb612
akatriel <michael.c.gabay@gmail.com>
2017-01-24 20:18:19 -0500

    redid stock model for stock_quote gem. successfully displayed data on show page.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,36 +1,4 @@
 class Stock < ApplicationRecord
 	has_many :assets
 	has_many :users, through: :assets
-
-
-
-	def self.get_fool ticker
-		ticker = ticker.upcase
-		apikey = ENV["FOOL_KEY"]
-		url = "http://www.fool.com/a/caps/ws/Ticker/#{ticker}?apikey=#{apikey}"
-		# begin
-			HTTParty.get url
-		# rescue
-			# return false
-		# end
-	end
-
-	Quandl::ApiConfig.api_key = 'exAUgh8NLoYAjuwd22PH'
-	Quandl::ApiConfig.api_version = '2015-04-09'
-	# @database = Quandl::Database.get('WIKI')	
-
-	def self.get_wiki ticker
-		ticker = ticker.upcase
-		begin
-			query = Quandl::Dataset.get("WIKI/#{ticker.upcase}").data.first
-			cloned = query.clone
-			cloned
-		rescue
-			return false
-		end
-	end
-
-	def self.get_from_yahoo ticker
-
-	end
 end
